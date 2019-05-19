e24c753c2bb6d770bca2df71090d2bc323dd46cc
Ruth Baker <ruthmoog@gmail.com>
2019-05-19 17:44:46 +0100

    Make censored tweets function case insensitive

--- blank_house.rb
+++ blank_house.rb
@@ -14,6 +14,9 @@ CENSOR_WORD = "CENSORED"
 
 def censor_tweets(tweets, banned_phrases)
   tweets.each do |tweet|
-    banned_phrases.each { |phrase| tweet.gsub!(phrase, CENSOR_WORD) }
+    banned_phrases.each do |phrase| 
+      pattern = Regexp.new(phrase, Regexp::IGNORECASE)
+      tweet.gsub!(pattern, CENSOR_WORD)
+    end
   end
 end
--- blank_house_spec.rb
+++ blank_house_spec.rb
@@ -1,16 +1,22 @@
 require "./blank_house.rb"
 
 describe "blank house" do
-  it "works" do
-    expect(true).to(eq(true))
-  end
 
+  banned_phrases = ["bad", "danger to society"]
+  
   it "censors multiple phrases in multiple tweets" do
     tweets = ["President bad is a danger to society", "This danger to society is president?!", "The blank house is bad. Very bad."]
-    banned_phrases = ["bad", "danger to society"]
-    
+
     censored_tweets = ["President #{CENSOR_WORD} is a #{CENSOR_WORD}", "This #{CENSOR_WORD} is president?!", "The blank house is #{CENSOR_WORD}. Very #{CENSOR_WORD}."]
 
     expect(censor_tweets(tweets, banned_phrases)).to(eq(censored_tweets))
+  end
+
+  it "censors regardless of letter case" do
+    mixed_case_tweets = ["President bAd is a danGer to SOCIETY!"]
+
+    censored_tweets = ["President #{CENSOR_WORD} is a #{CENSOR_WORD}!"]
+
+    expect(censor_tweets(mixed_case_tweets, banned_phrases)).to(eq(censored_tweets))
   end
 end
