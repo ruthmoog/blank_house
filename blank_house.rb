030f26352b78a94454f9990cb9cbf6fea532761f
Ruth Baker <ruthmoog@gmail.com>
2019-05-24 16:41:03 +0100

    Remove objective from the blank_house.rb file

--- blank_house.rb
+++ blank_house.rb
@@ -1,5 +1,3 @@
-# I'm a client working for the Blank House. We want to display positive tweets about our president on our website. However, our president is kind of unpopular, and we pretty much only receive negative press. Write me a program that filters out the following words from tweets: "sucks", "bad", "hate", "foolish", and the most popular: "danger to society". Replace each negative word or phrase them with the word "CENSORED". Some test tweets have been provided for you.
-
 require "./censor_tweets.rb"
 
 test_tweets = [
